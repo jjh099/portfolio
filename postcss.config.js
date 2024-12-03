22b0a0fea6d139616d0eb01fdfde6c054984b7ae
jjh <wogml220723@naver.com>
2024-12-03 14:52:24 +0900

    css 설정

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+
+// PostCSS 플러그인을 관리하며,
+// Tailwind CSS 실행,
+// CSS 자동 접두사 추가,
+// CSS 최적화 등의 역할을 수행
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// css 커스터마이징 하는 파일
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
