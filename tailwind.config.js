13b8c09bd8d4d9cb0c873f874b1a407724f23dd3
jjh <wogml220723@naver.com>
2024-12-13 16:30:49 +0900

    컬러 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,27 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#ffffff",
+        black: "#030303",
+        line: "#4d4d4d",
+        gray: {
+          1: "#fbfbfb",
+          2: "#eeeeee",
+          3: "#ececec",
+          4: "#e6e6e6",
+          5: "#dfdfdf",
+          6: "#cccccc",
+          7: "#b3b3b3",
+          8: "#999999",
+          9: "#767676",
+          10: "#666666",
+          11: "#242424",
+          12: "#151515",
+        },
+      },
+    },
   },
   plugins: [],
 };
