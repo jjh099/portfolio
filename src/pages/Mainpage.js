fb7bcea102579cb11583a1978e8c3a9614b3710c
jjh <wogml220723@naver.com>
2024-12-13 18:01:27 +0900

    폰트 설정

--- src/pages/Mainpage.js
+++ src/pages/Mainpage.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function MainPage(props) {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="h1">H1</h1>
+      <h1 className="sbt1">SBT1</h1>
+      <p className="body1">Body1</p>
+      <p className="main-title">Welcom</p>
+      <p className="menu">menu</p>
+    </div>
+  );
 }
 
 export default MainPage;
